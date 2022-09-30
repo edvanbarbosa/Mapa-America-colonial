55209d191dff71cfe425bea031e798e5bdc5f14a
Edvan Barbosa <114129267+edvanbarbosa@users.noreply.github.com>
2022-09-30 19:10:33 -0300

    primeiro template

--- script/script1.js
+++ script/script1.js
@@ -5,18 +5,21 @@ bt_artigo.addEventListener('click',openartigo)
 bt_mapa.addEventListener('click',openmapa)
 
 function openartigo(){
+    bt_mapa.style.borderBottom = '3px solid transparent'
     document.querySelector('div#Mapa').style.display = 'none'
     document.querySelector('div.artigo').style.display = 'block' //verificar depois se display = 'flex'
     bt_artigo.style.fontWeight = 'bold'
     bt_mapa.style.fontWeight = 'normal'
     bt_artigo.style.borderBottom = '3px solid #f3b60d'
-    bt_mapa.style.borderBottom = '3px solid transparent'
+ 
+    
 }
 function openmapa(){
+    bt_artigo.style.borderBottom = '3px solid transparent'
     document.querySelector('div#Mapa').style.display = 'flex'
     document.querySelector('div.artigo').style.display = 'none'
     bt_mapa.style.fontWeight = 'bold'
     bt_artigo.style.fontWeight = 'normal'
     bt_mapa.style.borderBottom = '3px solid #f3b60d'
-    bt_artigo.style.borderBottom = '3px solid transparent'
+
 }
